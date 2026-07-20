33da42644b112455e31b8c58c686336bb1d33ef0
Kotapati Dhananjay <your-email@example.com>
2026-07-20 23:15:10 +0530

    Initial commit

---
+++ Task 1.py
@@ -0,0 +1,10 @@
+num1 = float(input("Enter the first number : "))
+num2 = float(input("Enter the second number : "))
+addition = num1 + num2
+subtraction = num1 - num2
+multiplacation = num1 * num2
+division = num1 / num2
+print("Addition is",addition)
+print("Subtraction is",subtraction)
+print("Multiplication is",multiplacation)
+print("Division is",division)
